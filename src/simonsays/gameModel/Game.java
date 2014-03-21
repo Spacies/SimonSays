87879313a10d96b91f1ec212bfa9e2ee6858e4e0
Spacies <onesixzerofive@gmail.com>
2014-03-22 12:14:18 +1300

    Moved input code from Game class to input class. Created a game state enum variable with temporary values to be used in later versions of the game

--- src/simonsays/gameModel/Game.java
+++ src/simonsays/gameModel/Game.java
@@ -26,10 +26,6 @@ import java.util.logging.Logger;
 public class Game 
 {    
     
-    TestSoundGeneration testSoundC;
-    TestSoundGeneration testSoundE;
-    TestSoundGeneration testSoundG;
-    TestSoundGeneration testSoundB;
     
     /**
      * A new instance of Simon Says that begins producing output and 
@@ -37,65 +33,6 @@ public class Game
      */ 
     public Game() 
     {
-        
-        //testSoundC = new TestSoundGeneration("file:C4_262Hz_1S.wav");
-        //TestSoundGeneration testSoundE = new TestSoundGeneration("file:E4_330Hz_1S.wav");
-        //TestSoundGeneration testSoundG = new TestSoundGeneration("file:G4_392Hz_1S.wav");
-        //TestSoundGeneration testSoundB = new TestSoundGeneration("file:B3_247Hz_1S.wav");
-        //TestSoundGeneration testSoundCAgain = new TestSoundGeneration("file:C4_262Hz_1S.wav");
-        //Temporary test of output object/output methods
-        Output output = new Output();
-        receiveInput();
-        
+        Output output = new Output();  
     }
-    
-    /**
-     * Code obtained from web - Input is typed into command line and then
-     * stored as a string. Input is shown in command line as text, can 
-     * abort through typing "quit" (See while clause)
-     * 
-     * Code complains re: declaring IOException, Netbeans suggests 
-     * try-catch clause and automatically formatted.
-     * 
-     * Enter moves to next line and previous input remains. This will
-     * continue until "Quit" is entered
-     */
-    private void receiveInput() 
-    {
-        //Automatic fix by netbeans to handle IOExceptions using try-catch
-        try 
-        {         
-            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-            String line = "";
-            //Creates user-defined exit from method
-            while (line.equalsIgnoreCase("quit") == false)
-            {
-                //Stores the typed input as a String variable
-                line = in.readLine(); 
-                
-                // Check for press of button and play appropriate tone.
-                // Need to press enter to pass input
-                if (line.equals("1"))
-                    testSoundC = new TestSoundGeneration("file:C4_262Hz_1S.wav");
-                else if (line.equals("3"))
-                    testSoundE = new TestSoundGeneration("file:E4_330Hz_1S.wav");
-                else if (line.equals("5"))
-                    testSoundG = new TestSoundGeneration("file:G4_392Hz_1S.wav");
-                else if (line.equals("7"))
-                    testSoundB = new TestSoundGeneration("file:B3_247Hz_1S.wav");
-                
-                        
-                        
-                //Temporary means of clearing screen within the netbeans IDE
-                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
-                        + "\n\n\n");           
-            }         
-            in.close();
-        } 
-        catch (IOException ex) 
-        {
-            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }  
-    
 }
--- src/simonsays/gameModel/Input.java
+++ src/simonsays/gameModel/Input.java
@@ -6,6 +6,12 @@
 
 package simonsays.gameModel;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * This is a class that handles the input and the various rules involved    
  * with accepting the input in the SimonSays game
@@ -14,5 +20,68 @@ package simonsays.gameModel;
  * @version 22/03/2014
  */
 public class Input {
+    private TestSoundGeneration testSoundC;
+    private TestSoundGeneration testSoundE;
+    private TestSoundGeneration testSoundG;
+    private TestSoundGeneration testSoundB;
     
-}
+
+    public Input()
+    {
+        
+    }
+
+    
+    /**
+     * Code obtained from web - Input is typed into command line and then
+     * stored as a string. Input is shown in command line as text, can 
+     * abort through typing "quit" (See while clause)
+     * 
+     * Code complains re: declaring IOException, Netbeans suggests 
+     * try-catch clause and automatically formatted.
+     * 
+     * Enter moves to next line and previous input remains. This will
+     * continue until "Quit" is entered
+     */
+    private void receiveInput() 
+    {
+        //Automatic fix by netbeans to handle IOExceptions using try-catch
+        try 
+        {         
+            BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+            String line = "";
+            //Creates user-defined exit from method
+            while (line.equalsIgnoreCase("quit") == false)
+            {
+                //Stores the typed input as a String variable
+                line = in.readLine(); 
+
+                // Check for press of button and play appropriate tone.
+                // Need to press enter to pass input
+                if (line.equals("1"))
+                    testSoundC = new TestSoundGeneration("file:C4_262Hz_1S.wav");
+                else if (line.equals("3"))
+                    testSoundE = new TestSoundGeneration("file:E4_330Hz_1S.wav");
+                else if (line.equals("5"))
+                    testSoundG = new TestSoundGeneration("file:G4_392Hz_1S.wav");
+                else if (line.equals("7"))
+                    testSoundB = new TestSoundGeneration("file:B3_247Hz_1S.wav");
+
+
+
+                //Temporary means of clearing screen within the netbeans IDE
+                System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+                        + "\n\n\n");           
+            }         
+            in.close();
+        } 
+        catch (IOException ex) 
+        {
+            Logger.getLogger(Game.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+
+} 
+
+
