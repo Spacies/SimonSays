2279c8b6a4a28fb0acadb0cb91ebe298ae5796df
jaimesbooth <jaimesbooth@unknown-90-27-e4-f8-ca-f6.lan>
2014-03-23 11:49:59 +1300

    Changed audio Class to MakeSound. Implemented a pause so sounds can be played consecutively. git ingnored nbproject/private/; build/; dist/

--- src/simonsays/gameModel/Game.java
+++ src/simonsays/gameModel/Game.java
@@ -1,4 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
 package simonsays.gameModel;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * This is the class that knows the Simon Says game rules and enforces these
@@ -7,7 +19,7 @@ package simonsays.gameModel;
  * @author Jaimes Booth & Sam McGarvey
  * @version 17/03/2014
  * @modified Jaimes
- *  Added the call to MakeSound class for testing purposes.
+ *  Added the call to TestSoundGeneration class for testing purposes.
  *  Added keys 1,2,3,4 to correspond with different tones. Need to press enter 
  *  for program to register.
  */
